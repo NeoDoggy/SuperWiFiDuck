5b81b159c448eb3864583606438ad8bd9c19cec6
NeoDoggy <neodog0616@gmail.com>
2024-10-30 23:44:32 +0800

    update while loop

--- src/duckscript.cpp
+++ src/duckscript.cpp
@@ -1,26 +1,36 @@
 /*
    This software is licensed under the MIT License. See the license file for details.
    Source: https://github.com/spacehuhntech/WiFiDuck
- */
+*/
 
 #include "duckscript.h"
 #include "duckparser.h"
-
+// #include "Vector.h"
+#include <cppQueue.h>
 
 #include "config.h"
 #include "debug.h"
-
+#include "Queue.h"
 #include "spiffs.h"
 
 namespace duckscript {
 
     File f;
-
+    // Vector<String> prevNmsg;
+    // Vector<size_t> prevNmsgS;
+    // cppQueue	q(sizeof(String *), 256, FIFO);	// Instantiate queue
+    // cppQueue	qs(sizeof(size_t), 256, FIFO);	// Instantiate queue
+    Queue<String> q=Queue<String>();
+    Queue<size_t> qs=Queue<size_t>();
+    uint16_t ql=0;
+    int whilecnt=0;
+    bool dowhile=0;
     char * prevMessage    { NULL };
     size_t prevMessageLen { 0 };
+    String * pStr;
+    size_t tmps;
 
     bool running { false };
-
 
     static void runTask(void* parameter) {
         String* fileNamePtr = (String*) parameter;
@@ -80,6 +90,7 @@ namespace duckscript {
 
             if (!eol) debugln();
 
+
             if (strncmp((char*)buf, "REPEAT", _min(buf_i, 6)) == 0 || strncmp((char*)buf, "REPLAY", _min(buf_i, 6)) == 0) {
                 // Extract repeat count if available
                 char* pch = strtok((char*)buf + 6, " ");
@@ -94,17 +105,56 @@ namespace duckscript {
                         duckparser::parse(prevMessage, prevMessageLen);
                     }
                 }
-            } else {
+            }
+            else if (strncmp((char*)buf, "WHILEN", _min(buf_i, 6)) == 0 ){
+                // Extract repeat count if available
+                char* pch = strtok((char*)buf + 6, " ");
+                whilecnt = atoi(pch)<=0?1:atoi(pch);
+                q.clear();
+                qs.clear();
+                // q.clean();
+                // qs.clean();
+                ql=0;
+                dowhile=1;
+            }
+            else if (strncmp((char*)buf, "ENDWHL", _min(buf_i, 6)) == 0 ){
+                Queue<String> tmpq=q;
+                Queue<size_t> tmpqs=qs;
+                for (int i=0;i<whilecnt;++i) {
+                    while(tmpq.count()>0){
+                        String tmps=tmpq.pop();
+                        if(tmps)duckparser::parse(tmps.c_str(),tmpqs.pop());  
+                    }
+                }
+                q.clear();
+                qs.clear();
+                whilecnt=0;
+                // duckparser::parse(String(ql).c_str(),1);
+                ql=0;
+                dowhile=0;
+            }
+            else {
                 // Store this as the previous message
                 if (prevMessage) free(prevMessage);
                 prevMessageLen = buf_i;
                 prevMessage = (char*)malloc(prevMessageLen + 1);
                 memcpy(prevMessage, buf, buf_i);
                 prevMessage[buf_i] = '\0';
-
-                // Process the line here
-                duckparser::parse(buf, buf_i);
+                if(dowhile){
+                    ql++;
+                    String tmpbf(prevMessage);
+                    q.push(tmpbf);
+                    qs.push(prevMessageLen);
+                    // q.push(&tmpbf);
+                    // qs.push(&prevMessageLen);
+                    // prevNmsg.push_back(tmpbf);
+                    // prevNmsgS.push_back(prevMessageLen);
+                }
+                else{
+                    duckparser::parse(buf, buf_i);
+                }
             }
+            
         }
     }
 
